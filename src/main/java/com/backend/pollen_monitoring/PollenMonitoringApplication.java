43fbadf5571d5e5737af44a9f9166eebf1b3ec2b
HamCam203 <camarahamoudia@gmail.com>
2025-04-14 14:42:54 +0200

    first commit

---
+++ src/main/java/com/backend/pollen_monitoring/PollenMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pollen_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollenMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollenMonitoringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/pollen_monitoring/PollenMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.pollen_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollenMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
